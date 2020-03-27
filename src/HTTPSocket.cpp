81571c3f38a7d5a41407ddda4a001f112f3f7121
m.schlueter01 <m.schlueter01@stud.uni-goettingen.de>
2020-03-27 17:43:06 +0100

    forwards request with .apk

--- src/HTTPSocket.cpp
+++ src/HTTPSocket.cpp
@@ -69,6 +69,13 @@ int HTTPSocket::begin_request_handler(lh_ctx_t *ctx, lh_con_t *con)
 	for(int cnt=0;cnt < request_info->num_headers ; cnt++)
 		cout << "Header " << request_info->http_headers[cnt].name << "\t" << request_info->http_headers[cnt].value << endl;
 
+	// if path has apk in it its probably getting the file
+	std::string path = request_info->local_uri;
+	if(path.find(".apk")!=std::string::npos){
+		// set 0 so libhttp sends file
+		return 0;
+	}
+
 	if(req_method == "GET"){
 		if(socket->conhandler != nullptr){
 			socket->conhandler->get(&ich,request_info->local_uri);
@@ -93,7 +100,7 @@ int HTTPSocket::begin_request_handler(lh_ctx_t *ctx, lh_con_t *con)
 		if(socket->conhandler != nullptr){
 			socket->conhandler->del(&ich,request_info->local_uri);
 		}
-	}
+	}	
 
 	return 1;	// make shure that httpserver doesent try to load something
 }
