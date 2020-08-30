bfbf3e9d7e007b8bc3d1b3e49fc010c7663acfa8
m.schlueter01 <m.schlueter01@stud.uni-goettingen.de>
2020-08-30 16:35:45 +0200

    deleted not necessary build files

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marius/Code/restlesserver
-# Build directory: /home/marius/Code/restlesserver/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("modules/json")
--- build/modules/json/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marius/Code/restlesserver/modules/json
-# Build directory: /home/marius/Code/restlesserver/build/modules/json
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
