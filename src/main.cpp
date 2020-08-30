d800828b6cab8d67042b3befdd1c954120e2b2cc
m.schlueter01 <m.schlueter01@stud.uni-goettingen.de>
2020-08-30 19:46:17 +0200

    first building version with cmake

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ using namespace rls;
 int main(int argc,char *argv[])
 {
 	// report version
-    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
-              << Tutorial_VERSION_MINOR << std::endl;
+    std::cout << argv[0] << " Version " << RLSERV_VERSION_MAJOR << "."
+              << RLSERV_VERSION_MINOR << std::endl;
 
 	App *app = nullptr;
 	try{
