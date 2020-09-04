763eed518871a08c3dcda61b625db75152bbe0a1
m.schlueter01 <m.schlueter01@stud.uni-goettingen.de>
2020-09-04 12:58:54 +0200

    formating and added possibility to disable sending answer

--- rlservlib/App.cpp
+++ rlservlib/App.cpp
@@ -5,264 +5,287 @@ using namespace std;
 namespace rls
 {
 
-App::App()
-{
-	soc = nullptr;
-	ser = nullptr;
-	dbitem = nullptr;
-	dbuser = nullptr;
-	dbplaces = nullptr;
-}
-
-bool App::initialize(int argc,char *argv[])
-{
-	int port = PORT;
-	if (argc == 2){
-		port = atoi(argv[1]);
-	}
-	soc = dep.getHttpSocket();
-	ser = dep.getJSONSerializer();
-	dbitem = dep.getDataBaseItem();
-	dbuser = dep.getDataBaseUser();
-	dbplaces = dep.getDataBasePlaces();
-	soc->init(this, port);
-	return true;
-}
-
-void App::run()
-{
-	try
+	App::App()
 	{
-		cout << "Starting Server" << endl;
-		soc->run();
-		cout << "Server finished without exception" << endl;
+		soc = nullptr;
+		ser = nullptr;
+		dbitem = nullptr;
+		dbuser = nullptr;
+		dbplaces = nullptr;
 	}
-	catch (exception &exc)
+
+	bool App::initialize(int argc, char *argv[])
 	{
-		cerr << "Exception in Application trying to run soccet" << endl;
-		cerr << exc.what() << endl;
-	}
-}
-void App::ok(IConHandle* soc, std::string content)
-{
-	std::stringstream ss;
-	ss << "HTTP/1.1 200 OK\r\n";
-	ss << "Content-Type: application/json\r\n";
-	ss << "Content-Length: " << content.length() << "\r\n";
-	ss << "\r\n";
-	ss << content;
-
-	std::string message = ss.str();
-
-	cout << "Sending this:" << endl;
-	cout << message << endl;
-
-	soc->send(message); 
-}
-void App::nok(IConHandle* soc, std::string content){
-	std::stringstream ss;
-	ss << "HTTP/1.1 404 SomeError\r\n";
-	ss << "Content-Type: application/json\r\n";
-	ss << "Content-Length: " << content.length() << "\r\n";
-	ss << "\r\n";
-	ss << content;
-
-	std::string message = ss.str();
-
-	cout << "Sending this:" << endl;
-	cout << message << endl;
-
-	soc->send(message); 
-}
-bool App::get(IConHandle *soc, std::string path)
-{
-	// answer if item is requested
-	if (path.find("/api/item/") == 0)
-	{
-		int itemid = 0;
-
-		sscanf(path.c_str(), "/api/item/%d", &itemid);
-
-		Item it = dbitem->get(itemid);
-		std::string content = ser->toJSON(it);
-		ok(soc,content);
-
-		return true;
-	}
-
-	// answer if items are requested
-	if (path.find("/api/items") == 0)
-	{
-		std::vector<int> it = dbitem->getIDs();
-		std::string content = ser->toJSON(it);
-		ok(soc,content);
-		return true;
-	}
-
-	// answer if user is requested
-	if (path.find("/api/user/") == 0)
-	{
-		int userid = 0;
-
-		sscanf(path.c_str(), "/api/user/%d", &userid);
-
-		User it = dbuser->get(userid);
-		std::string content = ser->toJSON(it);
-		ok(soc,content);
-
-		return true;
-	}
-
-	// answer if users are requested
-	if (path.find("/api/users") == 0)
-	{
-		std::vector<int> it = dbuser->getIDs();
-		std::string content = ser->toJSON(it);
-		ok(soc,content);
-		return true;
-	}
-
-
-	// answer if place is requested
-	if (path.find("/api/place/") == 0)
-	{
-		int placeid = 0;
-
-		sscanf(path.c_str(), "/api/place/%d", &placeid);
-
-		Place it = dbplaces->get(placeid);
-		std::string content = ser->toJSON(it);
-		ok(soc,content);
-
-		return true;
-	}
-
-	// answer if place is requested
-	if (path.find("/api/places") == 0)
-	{
-		std::vector<int> it = dbplaces->getIDs();
-		std::string content = ser->toJSON(it);
-		ok(soc,content);
-		return true;
-	}
-
-	// Default answer
-
-	std::string content = "Hello from HelloSteimke RESTles API Server!\nRequested Path: '";
-	content += path + "'\n";
-	ok(soc,content);
-
-	return true;
-}
-bool App::put(IConHandle *soc, std::string path, std::string content)
-{
-	if(path.find("/api/item/") == 0){
-		Item it;
-		ser->fromJSON(content,&it);
-		dbitem->update(it);
-		ok(soc,"put item ok");
-		return true;
-	}else
-	if(path.find("/api/user/") == 0){
-		User it;
-		ser->fromJSON(content,&it);
-		dbuser->update(it);
-		ok(soc,"put user ok");
-		return true;
-	}else
-	if(path.find("/api/place/") == 0){
-		Place it;
-		ser->fromJSON(content,&it);
-		dbplaces->update(it);
-		ok(soc,"put place ok");
-		return true;
-	}else{
-		nok(soc,std::string("no handler (path:")+path+std::string(" ; type PUT)"));
-		return true;
-	}
-	return false;
-}
-bool App::post(IConHandle *soc, std::string path, std::string content)
-{
-	if(path.find("/api/item") == 0){
-		Item it;
-		ser->fromJSON(content,&it);
-
-		// check if valid 
-		User creator = dbuser->get(it.getCreatorID());
-		if(creator.getId() <= 0 || creator.getName().length() <= 3){
-			ok(soc,"");
-			return false;
+		int port = PORT;
+		if (argc == 2)
+		{
+			port = atoi(argv[1]);
 		}
-
-		dbitem->add(it);
-		ok(soc,"post item ok");
+		soc = dep.getHttpSocket();
+		ser = dep.getJSONSerializer();
+		dbitem = dep.getDataBaseItem();
+		dbuser = dep.getDataBaseUser();
+		dbplaces = dep.getDataBasePlaces();
+		soc->init(this, port);
 		return true;
-	}else
-	if(path.find("/api/user") == 0){
-		User it;
-		ser->fromJSON(content,&it);
+	}
 
-		// check if valid
-		User creator = it;
-		if(creator.getId() <= 0 || creator.getName().length() <= 3){
-			nok(soc,"not valid user");
+	void App::run()
+	{
+		try
+		{
+			cout << "Starting Server" << endl;
+			soc->run();
+			cout << "Server finished without exception" << endl;
+		}
+		catch (exception &exc)
+		{
+			cerr << "Exception in Application trying to run soccet" << endl;
+			cerr << exc.what() << endl;
+		}
+	}
+	void App::ok(IConHandle *soc, std::string content)
+	{
+		if (soc == nullptr)
+			return;
+
+		std::stringstream ss;
+		ss << "HTTP/1.1 200 OK\r\n";
+		ss << "Content-Type: application/json\r\n";
+		ss << "Content-Length: " << content.length() << "\r\n";
+		ss << "\r\n";
+		ss << content;
+
+		std::string message = ss.str();
+
+		cout << "Sending this:" << endl;
+		cout << message << endl;
+
+		soc->send(message);
+	}
+	void App::nok(IConHandle *soc, std::string content)
+	{
+		if (soc == nullptr)
+			return;
+
+		std::stringstream ss;
+		ss << "HTTP/1.1 404 SomeError\r\n";
+		ss << "Content-Type: application/json\r\n";
+		ss << "Content-Length: " << content.length() << "\r\n";
+		ss << "\r\n";
+		ss << content;
+
+		std::string message = ss.str();
+
+		cout << "Sending this:" << endl;
+		cout << message << endl;
+
+		soc->send(message);
+	}
+	bool App::get(IConHandle *soc, std::string path)
+	{
+		// answer if item is requested
+		if (path.find("/api/item/") == 0)
+		{
+			int itemid = 0;
+
+			sscanf(path.c_str(), "/api/item/%d", &itemid);
+
+			Item it = dbitem->get(itemid);
+			std::string content = ser->toJSON(it);
+			ok(soc, content);
+
 			return true;
 		}
 
-		dbuser->add(it);
-		ok(soc,"post user ok");
-		return true;
-	}else
-	if(path.find("/api/place") == 0){
-		Place it;
-		ser->fromJSON(content,&it);
-
-		// check if valid
-		User creator = dbuser->get(it.creatorId);
-		if(creator.getId() <= 0 || creator.getName().length() <= 3){
-			ok(soc,"");
-			return false;
+		// answer if items are requested
+		if (path.find("/api/items") == 0)
+		{
+			std::vector<int> it = dbitem->getIDs();
+			std::string content = ser->toJSON(it);
+			ok(soc, content);
+			return true;
 		}
 
-		dbplaces->add(it);
-		ok(soc,"post place ok");
-		return true;
-	} else{
-		nok(soc,std::string("no handler (path:")+path+std::string(" ; type POST)"));
+		// answer if user is requested
+		if (path.find("/api/user/") == 0)
+		{
+			int userid = 0;
+
+			sscanf(path.c_str(), "/api/user/%d", &userid);
+
+			User it = dbuser->get(userid);
+			std::string content = ser->toJSON(it);
+			ok(soc, content);
+
+			return true;
+		}
+
+		// answer if users are requested
+		if (path.find("/api/users") == 0)
+		{
+			std::vector<int> it = dbuser->getIDs();
+			std::string content = ser->toJSON(it);
+			ok(soc, content);
+			return true;
+		}
+
+		// answer if place is requested
+		if (path.find("/api/place/") == 0)
+		{
+			int placeid = 0;
+
+			sscanf(path.c_str(), "/api/place/%d", &placeid);
+
+			Place it = dbplaces->get(placeid);
+			std::string content = ser->toJSON(it);
+			ok(soc, content);
+
+			return true;
+		}
+
+		// answer if place is requested
+		if (path.find("/api/places") == 0)
+		{
+			std::vector<int> it = dbplaces->getIDs();
+			std::string content = ser->toJSON(it);
+			ok(soc, content);
+			return true;
+		}
+
+		// Default answer
+
+		std::string content = "Hello from HelloSteimke RESTles API Server!\nRequested Path: '";
+		content += path + "'\n";
+		ok(soc, content);
+
 		return true;
 	}
-	return false;
-}
-bool App::del(IConHandle *soc, std::string path)
-{
-	if(path.find("/api/item/") == 0){
-		int id = -1;
-		sscanf(path.c_str(),"/api/item/%d",&id);
-		dbitem->del(id);
-		ok(soc,"del item ok");
-		return true;
+	bool App::put(IConHandle *soc, std::string path, std::string content)
+	{
+		if (path.find("/api/item/") == 0)
+		{
+			Item it;
+			ser->fromJSON(content, &it);
+			dbitem->update(it);
+			ok(soc, "put item ok");
+			return true;
+		}
+		else if (path.find("/api/user/") == 0)
+		{
+			User it;
+			ser->fromJSON(content, &it);
+			dbuser->update(it);
+			ok(soc, "put user ok");
+			return true;
+		}
+		else if (path.find("/api/place/") == 0)
+		{
+			Place it;
+			ser->fromJSON(content, &it);
+			dbplaces->update(it);
+			ok(soc, "put place ok");
+			return true;
+		}
+		else
+		{
+			nok(soc, std::string("no handler (path:") + path + std::string(" ; type PUT)"));
+			return true;
+		}
+		return false;
 	}
-	if(path.find("/api/user/") == 0){
-		int id = -1;
-		sscanf(path.c_str(),"/api/user/%d",&id);
-		dbuser->del(id);
-		ok(soc,"del user ok");
-		return true;
+	bool App::post(IConHandle *soc, std::string path, std::string content)
+	{
+		if (path.find("/api/item") == 0)
+		{
+			Item it;
+			ser->fromJSON(content, &it);
+
+			// check if valid
+			User creator = dbuser->get(it.getCreatorID());
+			if (creator.getId() <= 0 || creator.getName().length() <= 3)
+			{
+				ok(soc, "");
+				return false;
+			}
+
+			dbitem->add(it);
+			ok(soc, "post item ok");
+			return true;
+		}
+		else if (path.find("/api/user") == 0)
+		{
+			User it;
+			ser->fromJSON(content, &it);
+
+			// check if valid
+			User creator = it;
+			if (creator.getId() <= 0 || creator.getName().length() <= 3)
+			{
+				nok(soc, "not valid user");
+				return true;
+			}
+
+			dbuser->add(it);
+			ok(soc, "post user ok");
+			return true;
+		}
+		else if (path.find("/api/place") == 0)
+		{
+			Place it;
+			ser->fromJSON(content, &it);
+
+			// check if valid
+			User creator = dbuser->get(it.creatorId);
+			if (creator.getId() <= 0 || creator.getName().length() <= 3)
+			{
+				ok(soc, "");
+				return false;
+			}
+
+			dbplaces->add(it);
+			ok(soc, "post place ok");
+			return true;
+		}
+		else
+		{
+			nok(soc, std::string("no handler (path:") + path + std::string(" ; type POST)"));
+			return true;
+		}
+		return false;
 	}
-	if(path.find("/api/place/") == 0){
-		int id = -1;
-		sscanf(path.c_str(),"/api/place/%d",&id);
-		dbplaces->del(id);
-		ok(soc,"del place ok");
-		return true;
+	bool App::del(IConHandle *soc, std::string path)
+	{
+		if (path.find("/api/item/") == 0)
+		{
+			int id = -1;
+			sscanf(path.c_str(), "/api/item/%d", &id);
+			dbitem->del(id);
+			ok(soc, "del item ok");
+			return true;
+		}
+		if (path.find("/api/user/") == 0)
+		{
+			int id = -1;
+			sscanf(path.c_str(), "/api/user/%d", &id);
+			dbuser->del(id);
+			ok(soc, "del user ok");
+			return true;
+		}
+		if (path.find("/api/place/") == 0)
+		{
+			int id = -1;
+			sscanf(path.c_str(), "/api/place/%d", &id);
+			dbplaces->del(id);
+			ok(soc, "del place ok");
+			return true;
+		}
+		return false;
 	}
-	return false;
-}
-App::~App()
-{
-	if (soc != nullptr)
-		soc->close();
-}
+	App::~App()
+	{
+		if (soc != nullptr)
+			soc->close();
+	}
 
 } // namespace rls
