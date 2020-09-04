8f7c3769aa06d7b21f7f236a31017a0a04689574
m.schlueter01 <m.schlueter01@stud.uni-goettingen.de>
2020-09-04 14:41:34 +0200

    added default constructor/destructor to mock

--- rlservlib/webservice/MockConHandle.cpp
+++ rlservlib/webservice/MockConHandle.cpp
@@ -3,6 +3,14 @@
 namespace rls
 {
 
+	MockConHandle::MockConHandle()
+	{
+	}
+
+	MockConHandle::~MockConHandle()
+	{
+	}
+
 	void MockConHandle::send(std::string str)
 	{
 		send_content.append(str);
--- rlservlib/webservice/MockConHandle.h
+++ rlservlib/webservice/MockConHandle.h
@@ -5,6 +5,8 @@ namespace rls
 	class MockConHandle : public IConHandle
 	{
 	public:
+		MockConHandle();
+		virtual ~MockConHandle();
 		virtual void send(std::string str) override;
 		std::string send_content;
 	};
