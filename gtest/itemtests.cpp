8316a56c964f4a45e6a7255b9a15cb9af41fd846
m.schlueter01 <m.schlueter01@stud.uni-goettingen.de>
2020-09-04 02:25:06 +0200

    created a meaningful test

--- gtest/itemtests.cpp
+++ gtest/itemtests.cpp
@@ -1,6 +1,23 @@
 #include "gtest/gtest.h"
+#include "DependencyService.h"
 
-TEST(TestSuiteName, TestName) {
-	EXPECT_EQ(1, 1);
-  // do something
+using namespace rls;
+
+TEST(TestItemDB, ItemAddGet) {
+	DependencyService depend;
+	auto db = depend.getDataBaseItem();
+	TimeStamp created,assigned,finished;
+	Item it(121,ItemState::created,"test","shortdesc","desc",1,2,3,4,5,created,assigned,finished);
+	db->add(it);
+	auto it2 = db->get(it.getID());
+	EXPECT_EQ(it.getID(), it2.getID());
+	EXPECT_EQ(it.getName(), it2.getName());
+	EXPECT_EQ(it.getShortDiscription(), it2.getShortDiscription());
+	EXPECT_EQ(it.getDescription(), it2.getDescription());
+	EXPECT_EQ(it.getAssignedID(), it2.getAssignedID());
+	EXPECT_EQ(it.getCreatorID(), it2.getCreatorID());
+	EXPECT_EQ(it.getPriority(), it2.getPriority());
+	EXPECT_EQ(it.getWorkload(), it2.getWorkload());
+	EXPECT_EQ(it.getPlaceID(), it2.getPlaceID());
+	// do something
 }
