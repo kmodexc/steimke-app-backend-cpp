d6e325d092fc0b4c7ae9e3539fc4a58ce0da1513
m.schlueter01 <m.schlueter01@stud.uni-goettingen.de>
2020-09-10 00:30:56 +0200

    added idtest

--- gtest/usertest.cpp
+++ gtest/usertest.cpp
@@ -74,3 +74,35 @@ TEST(UserTest,UserTestMod){
 	EXPECT_EQ(con.send_content,"HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 11\r\n\r\ndel user ok");
 	con.send_content.clear();
 }
+
+TEST(UserTest,UserTestIds){
+	try{
+			spdlog::basic_logger_mt("rlservlib","logs/basic-file.log");
+	}catch(spdlog::spdlog_ex){
+
+	}
+	SPDLOG_DEBUG("");
+	rls::App app;
+	char portstr[] = "20999";
+	char *arr[] = {nullptr, portstr};
+	EXPECT_TRUE(app.initialize(2, arr));
+	rls::MockConHandle con;
+
+	// create string rep of item
+	std::string contentstr = "";
+	rls::DependencyService dep;
+	auto jsonconv = dep.getJSONSerializer();
+	contentstr = jsonconv->toJSON(rls::User(23,"testuser","pw","em",rls::UserState::user,0));
+
+	EXPECT_TRUE(app.post(&con,"/api/user",contentstr));
+	EXPECT_EQ(con.send_content,"HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 12\r\n\r\npost user ok");
+	con.send_content.clear();
+
+	EXPECT_TRUE(app.get(&con,"/api/users"));
+	EXPECT_EQ(con.send_content,"HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 4\r\n\r\n[23]");
+	con.send_content.clear();
+
+	EXPECT_TRUE(app.del(&con,"/api/user/23"));
+	EXPECT_EQ(con.send_content,"HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 11\r\n\r\ndel user ok");
+	con.send_content.clear();
+}
